a92ba1c53acd95292760c090d0906f9ba5d15240
Chetan Panchal <chetanpanchal1453@gmail.com>
2024-06-08 10:11:05 -0400

    Updated HTTP Event Collector Token

--- splunk.js
+++ splunk.js
@@ -1,5 +1,5 @@
 const SPLUNK_URL = 'http://localhost:8088/services/collector';
-const SPLUNK_TOKEN = 'b6nskbwiubub78sabjbjspldyd';
+const SPLUNK_TOKEN = 'f28b8714-c267-4281-b3a3-899d8b53f701';
 
 app.post('/metrics', async (req, res) => {
   const metrics = req.body.metrics;
