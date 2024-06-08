cec04e1754fecf950fe5e4929c0ca83d499a296f
Chetan Panchal <chetanpanchal1453@gmail.com>
2024-06-08 10:15:33 -0400

    Modified the metrics API to forward metrics to Splunk

--- metrics-api/index.js
+++ metrics-api/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const SPLUNK_URL = 'http://localhost:8088/services/collector';
+const SPLUNK_TOKEN = 'f28b8714-c267-4281-b3a3-899d8b53f701';
 
 const app = express();
 const PORT = 3000;
@@ -16,6 +18,23 @@ app.post('/metrics', async (req, res) => {
       timestamp: metric.timestamp,
       value: metric.value,
       metric: { __name__: metric.type, job: req.body.application }
+    });
+  }
+
+  res.status(200).send('Metrics received');
+});
+
+app.post('/metrics', async (req, res) => {
+  const metrics = req.body.metrics;
+
+  for (const metric of metrics) {
+    await axios.post(SPLUNK_URL, {
+      event: metric,
+      sourcetype: '_json',
+      index: 'metrics',
+      fields: { application: req.body.application },
+    }, {
+      headers: { Authorization: `Splunk ${SPLUNK_TOKEN}` },
     });
   }
 
